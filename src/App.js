4a911bbef5dc951b82f899438c34c655d7df9a50
balayokesh <balayokeshmani@gmail.com>
2023-09-30 12:54:47 +0530

    implemented section 1 after navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+
 import logo from './images/logo.svg';
+import illustrationWorking from './images/illustration-working.svg';
 
 function App() {
   return (
-    <div className='container'>
-      <nav className='navbar navbar-expand-lg bg-white bg-light m-3'>
+    <div>
+      <nav className='navbar navbar-expand-lg bg-white bg-light mx-sm-5 m-3 px-sm-5 mt-4 mb-sm-5'>
         <a className='navbar-brand' href='#'><img src={logo} alt='Shortly' /></a>
         <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'></span>
@@ -25,10 +27,26 @@ function App() {
           <hr className='text-white m-3' />
           <div className='navbar-nav'>
             <button className='btn gray-violet'>Login</button>
-            <button className='btn px-4' id='sign-up-btn'>Sign Up</button>
+            <button className='btn px-4 cyan-btn'>Sign Up</button>
           </div>
         </div>
       </nav>
+
+      <section className='d-flex align-items-center ms-sm-5 ps-sm-5'>
+        <div className='me-sm-5'>
+          <h1 style={{color: 'var(--VeryDarkBlue)'}}>
+            More than just shorter links
+          </h1>
+          <p>
+            Build your brand’s recognition and get detailed insights
+            on how your links are performing.
+          </p>
+          <button className='btn px-4 py-2 cyan-btn' id='get-started-btn'>Get Started</button>
+        </div>
+        <div id='image-container'>
+          <img src={illustrationWorking} alt='Illustration working' />
+        </div>
+      </section>
     </div>
   );
 }
